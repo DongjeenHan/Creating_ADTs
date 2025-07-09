cae4cbb3a796aff5d2965e6b2537653d8d064d80
DongjeenHan <dongjeenhan@gmail.com>
2025-07-09 15:29:07 -0600

    Update StackADT.java

--- src/utilities/StackADT.java
+++ src/utilities/StackADT.java
@@ -10,27 +10,27 @@ public interface StackADT<T> {
     /**
      * Adds an element to the top.
      * @param element the element to add
-     * @throws NullPointerException if element is null
+     * @throws NullPointerException if <code>element</code> is <code>null</code>
      */
     void push(T element);
 
     /**
      * Removes and returns the top element.
      * @return the removed element
-     * @throws java.util.NoSuchElementException if stack is empty
+     * @throws EmptyQueueException if the stack is empty
      */
     T pop();
 
     /**
      * Returns the top element without removing it.
      * @return the top element
-     * @throws java.util.NoSuchElementException if stack is empty
+     * @throws EmptyQueueException if the stack is empty
      */
     T peek();
 
     /**
      * Checks if the stack is empty.
-     * @return true if empty, false otherwise
+     * @return <code>true</code> if the stack is empty; <code>false</code> otherwise
      */
     boolean isEmpty();
 
